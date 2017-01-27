c000e4ce91f01624d4c6a1d19afe2d0abba5486c
Marcus Johnson <bumblebritches57@gmail.com>
2017-01-27 10:46:12 -0500

    Reorganized file heirarchy
    Redid the Xcode project because it couldn’t cope with the rename, and
    fixed the SeekBits -> SkipBits issue

--- Utilities/ModernPNG.c
+++ Utilities/ModernPNG.c
@@ -1,25 +1,24 @@
-//
-//  main.c
-//  PNGDecoder
-//
-//  Created by Marcus Johnson on 7/27/16.
-//  Copyright © 2016 Marcus Johnson. All rights reserved.
-//
+#include "libModernPNG.h"
 
-#include "/usr/local/Packages/BitIO/include/BitIO.h"
-#include "../include/NewPNG.h"
-
-int main(int argc, const char * argv[]) {
-    BitInput  *BitI    = calloc(sizeof(BitInput), 1);
-    BitOutput *BitO    = calloc(sizeof(BitOutput), 1);
-    ErrorStatus *Error = calloc(sizeof(ErrorStatus), 1);
-    PNGDecoder *PNG    = calloc(sizeof(PNGDecoder), 1);
+#ifdef __cplusplus
+extern "C" {
+#endif
     
-    InitBitInput(BitI, Error, argc, argv);
-    InitBitOutput(BitO, Error, argc, argv);
-    InitPNGDecoder(PNG);
-    ParsePNG(BitI, PNG);
+    int main(int argc, const char * argv[]) {
+        BitInput  *BitI    = calloc(sizeof(BitInput), 1);
+        BitOutput *BitO    = calloc(sizeof(BitOutput), 1);
+        ErrorStatus *Error = calloc(sizeof(ErrorStatus), 1);
+        PNGDecoder *PNG    = calloc(sizeof(PNGDecoder), 1);
+        
+        InitBitInput(BitI, Error, argc, argv);
+        InitBitOutput(BitO, Error, argc, argv);
+        InitPNGDecoder(PNG);
+        ParsePNG(BitI, PNG);
+        
+        // DecodePNG(BitI, PNG);
+        return 0;
+    }
     
-    // DecodePNG(BitI, PNG);
-    return 0;
+#ifdef __cplusplus
 }
+#endif
