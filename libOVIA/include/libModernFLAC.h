80c117b97430a4f86febe24020867bc4b65f4a20
Marcus Johnson <bumblebritches57@gmail.com>
2017-03-23 13:53:32 -0400

    Various changes

--- libOVIA/include/libModernFLAC.h
+++ libOVIA/include/libModernFLAC.h
@@ -1,12 +1,13 @@
-#include "/usr/local/Packages/libBitIO/include/BitIO.h"
-
 #pragma once
+
+#ifndef LIBMODERNFLAC_LIBMODERNFLAC_H
+#define LIBMODERNFLAC_LIBMODERNFLAC_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-    extern enum FLACConstants {
+    enum FLACConstants {
         FLACMagic                                                   = 0x664c6143, // fLaC
         FLACFrameMagic                                              =     0x3FFE,
         FLACMaxChannels                                             =          8,
@@ -28,9 +29,9 @@ extern "C" {
         FLACMedizCatalogNumberSize                                  =        128,
         FLACMaxCoeffs                                               =        882,
         FLACMaxRicePartitions                                       =         16,
-    } FLACConstants;
+    };
 
-    extern enum FLACPictureTypes {
+    enum FLACPictureTypes {
         Other                                                       = 0,
         FileIcon                                                    = 1, // 32x32, PNG only
         OtherFileIcon                                               = 2,
@@ -52,9 +53,9 @@ extern "C" {
         Illustration                                                = 18,
         BandLogo                                                    = 19,
         PublisherLogo                                               = 20,
-    } FLACPictureTypes;
+    };
 
-    extern enum FLACSpeakerLayout {
+    enum FLACSpeakerLayout {
         Mono                                                        = 0,
         Left_Right                                                  = 1,
         Left_Right_Center                                           = 2,
@@ -66,21 +67,21 @@ extern "C" {
         Left_Diff                                                   = 8,
         Right_Diff                                                  = 9,
         Average_Diff                                                = 10,
-    } FLACSpeakerLayout;
+    };
 
-    extern enum FLACPredictionTypes {
+    enum FLACPredictionTypes {
         Verbatim                                                    = 0,
         Constant                                                    = 1,
         FixedLinear                                                 = 2,
         FIRLinear                                                   = 3,
-    } FLACPredictionTypes;
+    };
 
-    extern enum FLACResidualTypes {
+    enum FLACResidualTypes {
         Simple                                                      = 0,
         Complex                                                     = 1,
-    } FLACResidualTypes;
+    };
 
-    extern enum FLACMetadataTypes {
+    enum FLACMetadataTypes {
         StreamInfo                                                  = 0, // 0x30 = 0 in ascii, 0x31 = 1, etc.
         Padding                                                     = 1,
         Custom                                                      = 2, // Application aka App specific
@@ -88,174 +89,35 @@ extern "C" {
         VorbisComment                                               = 4,
         Cuesheet                                                    = 5,
         Picture                                                     = 6,
-    } FLACMetadataTypes;
+    };
 
-    extern enum FLACSubFrameTypes {
+    enum FLACSubFrameTypes {
         Subframe_Constant                                           =  0,
         Subframe_Verbatim                                           =  1,
         Subframe_Fixed                                              =  8,
         Subframe_LPC                                                = 63,
-    } FLACSubFrameTypes;
+    };
 
-    extern enum FLACBlockType {
+    enum FLACBlockType {
         FixedBlockSize    = 0,
         VariableBlockSize = 1,
-    } FLACBlockType;
+    };
 
-    typedef struct FLACStreamInfo {
-        uint16_t MinimumBlockSize:16; // in samples; must be at least 16
-        uint16_t MaximumBlockSize:16; // If these match than it's fixed blocksize format
-        uint32_t MinimumFrameSize:24;
-        uint32_t MaximumFrameSize:24;
-        uint8_t      CodedSampleRate; // 4 bits
-        uint32_t       SampleRate:20; // 20 bits used
-        uint8_t      CodedChannels:4; // 3 bits, add 1 to get the real value
-        uint8_t             Channels;
-        uint8_t      CodedBitDepth:6; // 5 bits, 4-32 bits per sample
-        uint32_t            BitDepth;
-        uint64_t  SamplesInStream:36; // 36 bits, all the samples in the stream; channel agnostic
-        uint8_t     MD5[FLACMD5Size]; // MD5
-    } FLACStreamInfo;
-
-    typedef struct CueSheetTrack {
-        uint64_t TrackOffset[FLACMaxCueTracks];         // samples from the beginning of the FLAC file to the first sample of the track
-        uint8_t  TrackNum[FLACMaxCueTracks];            // which track is this again?
-        uint8_t  ISRC[FLACMaxCueTracks][FLACISRCSize];            // the tracks's ISRC number.
-        bool     IsAudio[FLACMaxCueTracks];             // 1 for audio, 0 for data
-        bool     PreEmphasis[FLACMaxCueTracks];         // 1 for yes 0 for no.
-                                                        // 19 bytes of 0s
-        uint8_t  NumTrackIndexPoints;
-    } CueSheetTrack;
-
-    typedef struct FLACCueSheet {
-        char     CatalogID[128];
-        uint64_t LeadIn;              // in samples
-        bool     IsCD;                // 1 if it came from a CD; 0 otherwise
-                                      // 265 bytes of 0s to be skipped
-        uint8_t  NumTracks;           // the number of tracks, CD has a max of 100 (including lead out)
-
-        // Cuesheet_Track
-        //CueSheetTrack Track[FLACMaxCueTracks];  // Pointer to array of CueSheetTrack structs.
-
-        // How about we just do it normally?
-        // CUESHEET_TRACK
-        uint64_t TrackOffset[FLACMaxCueTracks];         // samples from the beginning of the FLAC file to the first sample of the track
-        uint8_t  TrackNum[FLACMaxCueTracks];            // which track is this again?
-        uint8_t  ISRC[FLACMaxCueTracks][12];            // the tracks's ISRC number.
-        bool     IsAudio[FLACMaxCueTracks];             // 1 for audio, 0 for data
-        bool     PreEmphasis[FLACMaxCueTracks];         // 1 for yes 0 for no.
-                                                        // 19 bytes of 0s
-        uint8_t  NumTrackIndexPoints[FLACMaxCueTracks];
-        // CUESHEET_TRACK
-
-
-        // Cuesheet_Track_Index
-        uint64_t       IndexOffset; // no idea. must be multiple of CD sector for cd sources
-        uint8_t      IndexPointNum;
-    } FLACCueSheet;
-
-    typedef struct FLACVorbisComment {
-        bool      VorbisFramingBit;
-        uint8_t   VendorTagSize;
-        char     *VendorTag;
-        uint8_t   NumUserTags; // Number of tags.
-        uint8_t  *UserTagSize; // array for each tag, that says the size of the tag
-        char     *UserTagString; // Array of tags.
-    } FLACVorbisComment;
-
-    typedef struct FLACSeekTable {
-        uint32_t NumSeekPoints;
-        uint64_t SampleInTargetFrame[FLACMaxSeekPoints]; // FIXME: Sample in the whole file, or sample in a specific frame?
-        uint64_t OffsetFrom1stSample[FLACMaxSeekPoints]; // in bytes
-        uint16_t     TargetFrameSize[FLACMaxSeekPoints];
-    } FLACSeekTable;
-
-    typedef struct FLACStream {
-        uint32_t BlockSize;
-        uint32_t SampleRate;
-        uint8_t  BitDepth;
-    } FLACStream;
-
-    typedef struct FLACPicture {
-        uint32_t PicType;
-        uint32_t MIMESize; // size of the MIME string in bytes
-        uint8_t  MIMEString[FLACMaxMIMEString];
-        uint32_t PicDescriptionSize;
-        uint8_t  PicDescriptionString[FLACMaxPicDescriptionString];
-        uint32_t Width;
-        uint32_t Height;
-        uint32_t BitDepth;
-        uint32_t ColorsUsed; // 0 for not paletted
-        uint32_t PictureSize;
-        uint32_t *PictureStart; // Pointer to the start of the picture
-    } FLACPicture;
-
-    typedef struct FLACSubFrame {
-        uint8_t SubFrameType:6;
-        bool    WastedBitsFlag:1;
-        uint8_t WastedBits:6; // Uses unary coding
-    } FLACSubFrame;
-
-    typedef struct FLACFrame {
-        bool          BlockType:1;
-        uint8_t       CodedSamplesInBlock;
-        uint16_t      BlockSize; // SamplesInBlock
-        uint8_t       CodedSampleRate:5;
-        uint32_t      SampleRate;
-        uint8_t       ChannelLayout:4;
-        uint8_t       CodedBitDepth:4;
-        uint8_t       BitDepth;
-        // If variable block size
-        uint64_t      SampleNumber:36;
-        // Otherwise
-        uint32_t      FrameNumber:31;
-        // End
-        FLACSubFrame *Sub;
-        uint8_t       FLACFrameCRC;
-        uint8_t       PartitionOrder;
-        uint8_t       CurrentPartition;
-        uint8_t       SamplesInPartition;
-    } FLACFrame;
-    
-    typedef struct RICEPartition {
-        uint8_t NumRICEPartitions:4;
-        uint8_t EscapeBitDepth:5;
-        uint8_t RICEParameter[FLACMaxRicePartitions];
-    } RICEPartition;
-    
-    typedef struct FLACMeta {
-        uint32_t           MetadataSize;
-        FLACStreamInfo    *StreamInfo;
-        FLACSeekTable     *Seek; // Because the user may want to skip around, but this has nothing to do with decoding.
-        FLACVorbisComment *Vorbis;
-        FLACCueSheet      *Cue;
-        FLACPicture       *Pic;
-    } FLACMeta;
-    
-    typedef struct FLACLPC {
-        uint8_t LPCOrder;
-        uint8_t LPCPrecision:4;
-        uint8_t LPCShift:5;
-        uint8_t NumLPCCoeffs;
-        int8_t  LPCCoeff[FLACMaxCoeffs];
-        uint8_t RicePartitionType:2;
-        uint8_t PartitionOrder:4;
-    } FLACLPC;
-    
-    typedef struct FLACData {
-        bool               GetSampleRateFromStreamInfo;
-        FLACFrame         *Frame;
-        FLACSubFrame      *SubFrame;
-        FLACLPC           *LPC;
-        RICEPartition     *Rice;
-        uint32_t           RAWAudio[FLACMaxChannels][FLACMaxSamplesInBlock];
-    } FLACData;
-    
-    extern enum FLACRicePartitionType {
+    enum FLACRicePartitionType {
         RICE1 = 0,
         RICE2 = 1,
-    } FLACRicePartitionType;
+    };
+    
+    typedef struct FLACEncoder EncodeFLAC;
+    
+    typedef struct FLACDecoder DecodeFLAC;
+    
+    EncodeFLAC *InitFLACEncoder(void);
+    
+    DecodeFLAC *InitFLACDecoder(void);
     
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* LIBMODERNFLAC_LIBMODERNFLAC_H */
