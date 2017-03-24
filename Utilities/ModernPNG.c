ece053ec8f7a006ca7d92dd0a13afe68ef783736
Marcus Johnson <bumblebritches57@gmail.com>
2017-03-24 00:03:47 -0400

    Stopped accessing the CMD struct directly

--- Utilities/ModernPNG.c
+++ Utilities/ModernPNG.c
@@ -33,16 +33,16 @@ extern "C" {
         CMD->Switch[1]->Switch            = "-o";
         CMD->Switch[1]->SwitchDescription = "Output file or stdout with: '-'\n";
         
-        CMD->Switch[2]->Switch            = "-d";
-        CMD->Switch[2]->SwitchDescription = "Decode PNG to output\n";
+        CMD->Switch[2]->Switch            = "-Res";
+        CMD->Switch[2]->SwitchDescription = "Resolution in WidthxHeight format\n";
         CMD->Switch[2]->Resultless        = true;
         
-        CMD->Switch[3]->Switch            = "-e";
-        CMD->Switch[3]->SwitchDescription = "Encode input to PNG\n";
+        CMD->Switch[3]->Switch            = "-d";
+        CMD->Switch[3]->SwitchDescription = "Decode PNG to output\n";
         CMD->Switch[3]->Resultless        = true;
         
-        CMD->Switch[4]->Switch            = "-Res";
-        CMD->Switch[4]->SwitchDescription = "Resolution in WidthxHeight format\n";
+        CMD->Switch[4]->Switch            = "-e";
+        CMD->Switch[4]->SwitchDescription = "Encode input to PNG\n";
         CMD->Switch[4]->Resultless        = true;
         
         CMD->Switch[5]->Switch            = "-Optimize";
@@ -57,21 +57,18 @@ extern "C" {
             DisplayCMDHelp(CMD);
         } else {
             ParseCommandLineArguments(CMD, argc, argv);
-            BitInput           *BitI  = calloc(sizeof(BitInput), 1);
-            BitOutput          *BitO  = calloc(sizeof(BitOutput), 1);
-            uint16_t           *DecodedImage = calloc(NULL, 1);
-            if        (CMD->Switch[2]->SwitchFound == true) {
-                PNGDecoder     *Dec   = InitPNGDecoder();
+            BitInput           *BitI = InitBitInput();
+            BitOutput          *BitO = InitBitOutput();
+            if        (IsSwitchPresent(CMD, 2) == true) {
+                // Split string into resolution.
+            } else if (IsSwitchPresent(CMD, 3) == true) {
+                DecodePNG     *Dec   = InitPNGDecoder();
                 DecodePNGImage(BitI, Dec, NULL);
-            } else if (CMD->Switch[3]->SwitchFound == true) {
-                PNGEncoder     *Enc   = InitPNGEncoder();
+            } else if (IsSwitchPresent(CMD, 4) == true) {
+                EncodePNG     *Enc   = InitPNGEncoder();
                 EncodePNGImage();
             }
-            if (CMD->Switch[4]->SwitchFound == true) {
-                // Split string into resolution.
-            }
         }
-        
         return EXIT_SUCCESS;
     }
     
