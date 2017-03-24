7337b86894abbd8b8df1b1883ca3a171dcc68103
Marcus Johnson <bumblebritches57@gmail.com>
2017-03-24 02:55:07 -0400

    Fixed InitPNG* functions.
    Fixed ModernPNG CMD stuff as well.

--- Utilities/ModernPNG.c
+++ Utilities/ModernPNG.c
@@ -8,64 +8,59 @@
 extern "C" {
 #endif
     
-    void SetModernPNGOptions(CommandLineOptions *CMD) {
-        CMD->NumSwitches                  = 5;
-        CMD->ProgramName                  = "ModernPNG";
-        CMD->ProgramDescription           = "PNG encoder/decoder written in modern C, from scratch";
-        CMD->AuthorCopyrightLicense       = "By BumbleBritches57, © 2017, Released under the BSD 3-clause license";
+    CommandLineOptions *SetModernPNGOptions(void) {
+        CommandLineOptions *CMD           = InitCommandLineOptions();
         
-        CLSwitch *Switch0                 = calloc(sizeof(CLSwitch), 1);
-        CLSwitch *Switch1                 = calloc(sizeof(CLSwitch), 1);
-        CLSwitch *Switch2                 = calloc(sizeof(CLSwitch), 1);
-        CLSwitch *Switch3                 = calloc(sizeof(CLSwitch), 1);
-        CLSwitch *Switch4                 = calloc(sizeof(CLSwitch), 1);
-        CLSwitch *Switch5                 = calloc(sizeof(CLSwitch), 1);
-        CMD->Switch[0]                    = Switch0;
-        CMD->Switch[1]                    = Switch1;
-        CMD->Switch[2]                    = Switch2;
-        CMD->Switch[3]                    = Switch3;
-        CMD->Switch[4]                    = Switch4;
-        CMD->Switch[5]                    = Switch5;
+        SetCMDName(CMD, "ModernPNG");
+        SetCMDDescription(CMD, "PNG encoder/decoder written in modern C, from scratch");
+        SetCMDAuthor(CMD, "BumbleBritches57");
+        SetCMDCopyright(CMD, "2017");
+        SetCMDLicense(CMD, "Revised BSD (3 clause)");
         
-        CMD->Switch[0]->Switch            = "-i";
-        CMD->Switch[0]->SwitchDescription = "Input file or stdin with: '-'\n";
+        BatchInitCommandLineSwitches(CMD, 6);
         
-        CMD->Switch[1]->Switch            = "-o";
-        CMD->Switch[1]->SwitchDescription = "Output file or stdout with: '-'\n";
+        SetSwitchFlag(CMD, 0, "i");
+        SetSwitchDescription(CMD, 0, "Input file or stdin with: '-'\n");
+        SetSwitchResultStatus(CMD, 0, false);
         
-        CMD->Switch[2]->Switch            = "-Res";
-        CMD->Switch[2]->SwitchDescription = "Resolution in WidthxHeight format\n";
-        CMD->Switch[2]->Resultless        = true;
+        SetSwitchFlag(CMD, 1, "o");
+        SetSwitchDescription(CMD, 1, "Output file or stdout with: '-'\n");
+        SetSwitchResultStatus(CMD, 1, false);
         
-        CMD->Switch[3]->Switch            = "-d";
-        CMD->Switch[3]->SwitchDescription = "Decode PNG to output\n";
-        CMD->Switch[3]->Resultless        = true;
+        SetSwitchFlag(CMD, 2, "Res");
+        SetSwitchDescription(CMD, 2, "Resolution in WidthxHeight format\n");
+        SetSwitchResultStatus(CMD, 2, false);
         
-        CMD->Switch[4]->Switch            = "-e";
-        CMD->Switch[4]->SwitchDescription = "Encode input to PNG\n";
-        CMD->Switch[4]->Resultless        = true;
+        SetSwitchFlag(CMD, 3, "d");
+        SetSwitchDescription(CMD, 3, "Decode PNG to output\n");
+        SetSwitchResultStatus(CMD, 3, true);
         
-        CMD->Switch[5]->Switch            = "-Optimize";
-        CMD->Switch[5]->SwitchDescription = "Optimize the encoded PNG to be as small as possible (try all filter options)\n";
-        CMD->Switch[5]->Resultless        = true;
+        SetSwitchFlag(CMD, 4, "e");
+        SetSwitchDescription(CMD, 4, "Encode input to PNG\n");
+        SetSwitchResultStatus(CMD, 4, true);
+        
+        SetSwitchFlag(CMD, 5, "Optimize");
+        SetSwitchDescription(CMD, 5, "Optimize the encoded PNG to be as small as possible (try all filter options)\n");
+        SetSwitchResultStatus(CMD, 5, true);
+        
+        return CMD;
     }
     
     int main(int argc, const char * argv[]) {
-        CommandLineOptions *CMD   = calloc(sizeof(CommandLineOptions), 1);
-        SetModernPNGOptions(CMD);
+        CommandLineOptions *CMD = SetModernPNGOptions();
         if (argc < 5) {
             DisplayCMDHelp(CMD);
         } else {
             ParseCommandLineArguments(CMD, argc, argv);
-            BitInput           *BitI = InitBitInput();
+            BitInput           *BitI =  InitBitInput();
             BitOutput          *BitO = InitBitOutput();
             if        (IsSwitchPresent(CMD, 2) == true) {
                 // Split string into resolution.
             } else if (IsSwitchPresent(CMD, 3) == true) {
-                DecodePNG     *Dec   = InitPNGDecoder();
+                DecodePNG     *Dec   = InitDecodePNG();
                 DecodePNGImage(BitI, Dec, NULL);
             } else if (IsSwitchPresent(CMD, 4) == true) {
-                EncodePNG     *Enc   = InitPNGEncoder();
+                EncodePNG     *Enc   = InitEncodePNG();
                 EncodePNGImage();
             }
         }
