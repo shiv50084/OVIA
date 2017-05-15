db1c4cdaf03bacfcda58220a409713abae271d69
Marcus Johnson <bumblebritches57@gmail.com>
2017-05-15 05:48:11 -0400

    Updated function calls

--- Utilities/ModernPNG.c
+++ Utilities/ModernPNG.c
@@ -20,33 +20,33 @@ extern "C" {
         SetCMDLicenseURL(CMD, "https://opensource.org/licenses/BSD-3-Clause", false);
         SetCMDMinSwitches(CMD, 3);
         
-        SetSwitchFlag(CMD, 0, "Input", 5);
-        SetSwitchDescription(CMD, 0, "Input file or stdin with: -");
-        SetSwitchResultStatus(CMD, 0, false);
+        SetCMDSwitchFlag(CMD, 0, "Input", 5);
+        SetCMDSwitchDescription(CMD, 0, "Input file or stdin with: -");
+        SetCMDSwitchResultStatus(CMD, 0, false);
         
-        SetSwitchFlag(CMD, 1, "Output", 6);
-        SetSwitchDescription(CMD, 1, "Output file or stdout with: -");
-        SetSwitchResultStatus(CMD, 1, false);
+        SetCMDSwitchFlag(CMD, 1, "Output", 6);
+        SetCMDSwitchDescription(CMD, 1, "Output file or stdout with: -");
+        SetCMDSwitchResultStatus(CMD, 1, false);
         
-        SetSwitchFlag(CMD, 2, "Resolution", 10);
-        SetSwitchDescription(CMD, 2, "Resolution in WidthxHeight format");
-        SetSwitchResultStatus(CMD, 2, false);
+        SetCMDSwitchFlag(CMD, 2, "Resolution", 10);
+        SetCMDSwitchDescription(CMD, 2, "Resolution in WidthxHeight format");
+        SetCMDSwitchResultStatus(CMD, 2, false);
         
-        SetSwitchFlag(CMD, 3, "Encode", 6);
-        SetSwitchDescription(CMD, 3, "Encode input to PNG");
-        SetSwitchResultStatus(CMD, 3, true);
+        SetCMDSwitchFlag(CMD, 3, "Encode", 6);
+        SetCMDSwitchDescription(CMD, 3, "Encode input to PNG");
+        SetCMDSwitchResultStatus(CMD, 3, true);
         
-        SetSwitchFlag(CMD, 4, "Decode", 6);
-        SetSwitchDescription(CMD, 4, "Decode PNG to output");
-        SetSwitchResultStatus(CMD, 4, true);
+        SetCMDSwitchFlag(CMD, 4, "Decode", 6);
+        SetCMDSwitchDescription(CMD, 4, "Decode PNG to output");
+        SetCMDSwitchResultStatus(CMD, 4, true);
         
-        SetSwitchFlag(CMD, 5, "Optimize", 8); // Should quotes be included with the string in strlen?
-        SetSwitchDescription(CMD, 5, "Optimize the encoded PNG to be as small as possible (try all filter options)");
-        SetSwitchResultStatus(CMD, 5, true);
+        SetCMDSwitchFlag(CMD, 5, "Optimize", 8); // Should quotes be included with the string in strlen?
+        SetCMDSwitchDescription(CMD, 5, "Optimize the encoded PNG to be as small as possible (try all filter options)");
+        SetCMDSwitchResultStatus(CMD, 5, true);
         
-        SetSwitchFlag(CMD, 6, "Help", 4);
-        SetSwitchDescription(CMD, 6, "Prints all the command line options");
-        SetSwitchResultStatus(CMD, 6, true);
+        SetCMDSwitchFlag(CMD, 6, "Help", 4);
+        SetCMDSwitchDescription(CMD, 6, "Prints all the command line options");
+        SetCMDSwitchResultStatus(CMD, 6, true);
         
         // Switch 6 is NULL, aka the auto Help option. Fuck it, the user will just have to code that themselves.
         
@@ -64,12 +64,12 @@ extern "C" {
         // We should add a function to BitIO that can tell us if an input command is a network address or a file.
         
         ParseCommandLineArguments(CMD, argc, argv);
-        if        (GetSwitchPresence(CMD, 2) == true) {
+        if        (GetCMDSwitchPresence(CMD, 2) == true) {
             // Split string into resolution.
-        } else if (GetSwitchPresence(CMD, 3) == true) { // Encode
+        } else if (GetCMDSwitchPresence(CMD, 3) == true) { // Encode
             EncodePNG     *Enc   = InitEncodePNG();
             PNGEncodeImage(Enc, BitO);
-        } else if (GetSwitchPresence(CMD, 4) == true) { // Decode
+        } else if (GetCMDSwitchPresence(CMD, 4) == true) { // Decode
             DecodePNG     *Dec   = InitDecodePNG();
             PNGDecodeImage(InputBitB, Dec, NULL);
         }
