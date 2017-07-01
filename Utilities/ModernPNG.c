c936b5d2e942fa3fca8348fab049ff104e081b35
Marcus Johnson <bumblebritches57@gmail.com>
2017-07-01 06:14:03 -0400

    Rewrote the utility for the new CommandLineInterface

--- Utilities/ModernPNG.c
+++ Utilities/ModernPNG.c
@@ -1,3 +1,4 @@
+#include "../Dependencies/BitIO/libBitIO/include/CommandLineInterface.h"
 #include "../Dependencies/BitIO/libBitIO/include/BitIO.h"
 
 #include "../libModernPNG/include/libModernPNG.h"
@@ -8,95 +9,143 @@
 extern "C" {
 #endif
     
-    CommandLineOptions *SetModernPNGOptions(void) {
-        CommandLineOptions *CMD = InitCommandLineOptions(12);
+    enum CommandLineSwitchNames {
+        Input      = 0,
+        Output     = 1,
+        LeftEye    = 2,
+        RightEye   = 3,
+        Encode     = 4,
+        Resolution = 5,
+        Interlace  = 6,
+        Optimize   = 7,
+        Combine3D  = 8,
+        Decode     = 9,
+        Split3D    = 10,
+        Help       = 11,
+    };
+    
+    CommandLineInterface *SetModernPNGOptions(void) {
+        CommandLineInterface *CLI = InitCommandLineInterface(12);
         
-        SetCMDName(CMD, "ModernPNG");
-        SetCMDVersion(CMD, ModernPNGVersion);
-        SetCMDAuthor(CMD, "BumbleBritches57");
-        SetCMDCopyright(CMD, "2017-2017");
-        SetCMDDescription(CMD, "PNG encoder/decoder written from scratch in modern C");
-        SetCMDLicense(CMD, "Revised BSD (3 clause)", false);
-        SetCMDLicenseURL(CMD, "https://opensource.org/licenses/BSD-3-Clause", false);
-        SetCMDMinSwitches(CMD, 3);
+        SetCLIName(CLI, "ModernPNG");
+        SetCLIAuthor(CLI, "BumbleBritches57");
+        SetCLIVersion(CLI, ModernPNGVersion);
+        SetCLICopyright(CLI, "2017 - 2017");
+        SetCLIDescription(CLI, "PNG encoder/decoder written from scratch in modern C");
+        SetCLILicense(CLI, "Revised BSD (3 clause)", false);
+        SetCLILicenseURL(CLI, "https://opensource.org/licenses/BSD-3-Clause", false);
+        SetCLIMinSwitches(CLI, 3);
         
-        SetCMDSwitchFlag(CMD, 0, "Input", 5);
-        SetCMDSwitchDescription(CMD, 0, "Input file or stdin with: -");
-        SetCMDSwitchResultStatus(CMD, 0, true);
+        SetCLISwitchFlag(CLI, Input, "Input", 5);
+        SetCLISwitchDescription(CLI, Input, "Input file or stdin with: -");
+        SetCLISwitchResultStatus(CLI, Input, true);
         
-        SetCMDSwitchFlag(CMD, 1, "Output", 6);
-        SetCMDSwitchDescription(CMD, 1, "Output file or stdout with: -");
-        SetCMDSwitchResultStatus(CMD, 1, true);
+        SetCLISwitchFlag(CLI, Output, "Output", 6);
+        SetCLISwitchDescription(CLI, Output, "Output file or stdout with: -");
+        SetCLISwitchResultStatus(CLI, Output, true);
         
-        SetCMDSwitchFlag(CMD, 2, "LeftEye", 7); // lefteye/righteye is just a piece of metadata to associate with input/output files
-        SetCMDSwitchDescription(CMD, 2, "The left view for encoding or decoding");
-        SetCMDSwitchResultStatus(CMD, 2, true);
+        SetCLISwitchFlag(CLI, LeftEye, "LeftEye", 7);
+        SetCLISwitchDescription(CLI, LeftEye, "The left view for encoding or decoding");
+        SetCLISwitchResultStatus(CLI, LeftEye, true);
         
-        SetCMDSwitchFlag(CMD, 3, "RightEye", 8);
-        SetCMDSwitchDescription(CMD, 3, "The right view for encoding or decoding");
-        SetCMDSwitchResultStatus(CMD, 3, true);
+        SetCLISwitchFlag(CLI, RightEye, "RightEye", 8);
+        SetCLISwitchDescription(CLI, RightEye, "The right view for encoding or decoding");
+        SetCLISwitchResultStatus(CLI, RightEye, true);
         
         /* Start Encode Options */
-        SetCMDSwitchFlag(CMD, 4, "Encode", 6);
-        SetCMDSwitchDescription(CMD, 4, "Encode input to PNG");
-        SetCMDSwitchResultStatus(CMD, 4, false);
+        SetCLISwitchFlag(CLI, Encode, "Encode", 6);
+        SetCLISwitchDescription(CLI, Encode, "Encode input to PNG");
+        SetCLISwitchResultStatus(CLI, Encode, false);
         
-        SetCMDSwitchFlag(CMD, 5, "Resolution", 10);
-        SetCMDSwitchDescription(CMD, 5, "Resolution in WidthxHeight format (if 3D specify the per eye resolution)");
-        SetCMDSwitchResultStatus(CMD, 5, true);
+        SetCLISwitchFlag(CLI, Resolution, "Resolution", 10);
+        SetCLISwitchDescription(CLI, Resolution, "Resolution in WidthxHeight format (if 3D specify the per eye resolution)");
+        SetCLISwitchResultStatus(CLI, Resolution, true);
         
-        SetCMDSwitchFlag(CMD, 6, "Interlace", 10);
-        SetCMDSwitchDescription(CMD, 6, "Resolution in WidthxHeight format (if 3D specify the per eye resolution)");
-        SetCMDSwitchResultStatus(CMD, 6, true);
+        SetCLISwitchFlag(CLI, Interlace, "Interlace", 10);
+        SetCLISwitchDescription(CLI, Interlace, "Resolution in WidthxHeight format (if 3D specify the per eye resolution)");
+        SetCLISwitchResultStatus(CLI, Interlace, true);
         
-        SetCMDSwitchFlag(CMD, 7, "Optimize", 8);
-        SetCMDSwitchDescription(CMD, 7, "Optimize the encoded PNG to be as small as possible (try all filter options)");
-        SetCMDSwitchResultStatus(CMD, 7, false);
+        SetCLISwitchFlag(CLI, Optimize, "Optimize", 8);
+        SetCLISwitchDescription(CLI, Optimize, "Optimize the encoded PNG to be as small as possible (try all filter options)");
+        SetCLISwitchResultStatus(CLI, Optimize, false);
         
-        SetCMDSwitchFlag(CMD, 8, "Combine3D", 8);
-        SetCMDSwitchDescription(CMD, 8, "Encode an image as a single stereoscopic, 3D image (the first option should be the left eye)");
-        SetCMDSwitchResultStatus(CMD, 8, false);
+        SetCLISwitchFlag(CLI, Combine3D, "Combine3D", 8);
+        SetCLISwitchDescription(CLI, Combine3D, "Encode an image as a single stereoscopic, 3D image (the first option should be the left eye)");
+        SetCLISwitchResultStatus(CLI, Combine3D, false);
         /* End Encode Options */
         
         /* Start Decode Options */
-        SetCMDSwitchFlag(CMD, 9, "Decode", 6);
-        SetCMDSwitchDescription(CMD, 9, "Decode PNG to output");
-        SetCMDSwitchResultStatus(CMD, 9, false);
+        SetCLISwitchFlag(CLI, Decode, "Decode", 6);
+        SetCLISwitchDescription(CLI, Decode, "Decode PNG to output");
+        SetCLISwitchResultStatus(CLI, Decode, false);
         
-        SetCMDSwitchFlag(CMD, 10, "Split3D", 6);
-        SetCMDSwitchDescription(CMD, 10, "Split 3D PNG to 2 output files");
-        SetCMDSwitchResultStatus(CMD, 10, false);
+        SetCLISwitchFlag(CLI, Split3D, "Split3D", 6);
+        SetCLISwitchDescription(CLI, Split3D, "Split 3D PNG to 2 output files");
+        SetCLISwitchResultStatus(CLI, Split3D, false);
         /* End Decode Options */
         
-        SetCMDSwitchFlag(CMD, 11, "Help", 4);
-        SetCMDSwitchDescription(CMD, 11, "Prints all the command line options");
-        SetCMDSwitchResultStatus(CMD, 11, false);
+        SetCLISwitchFlag(CLI, Help, "Help", 4);
+        SetCLISwitchDescription(CLI, Help, "Prints all the command line options");
+        SetCLISwitchResultStatus(CLI, Help, false);
         
-        return CMD;
+        return CLI;
     }
     
     int main(int argc, const char * argv[]) {
-        CommandLineOptions *CMD  = SetModernPNGOptions();
-        BitInput           *BitI = InitBitInput();
-        BitOutput          *BitO = InitBitOutput();
+        CommandLineInterface *CLI = SetModernPNGOptions();
+        ParseCommandLineArguments(CLI, argc, argv);
         
-        BitBuffer          *InputBitB = InitBitBuffer();
-        BitBuffer          *OutputBitB = InitBitBuffer();
+        if (GetCLISwitchPresence(CLI, Combine3D) == true) {
+            // 3D image, so check for LeftEye/RightEye
+            BitInput  *Lefteye    = InitBitInput();
+            BitInput  *Righteye   = InitBitInput();
+            BitOutput *Stereo     = InitBitOutput();
+            
+            uint64_t LeftEyeArg   = GetCLIMetaSwitchArgument(CLI, Combine3D, LeftEye);
+            uint64_t RightEyeArg  = GetCLIMetaSwitchArgument(CLI, Combine3D, RightEye);
+            uint64_t StereoArg    = GetCLIMetaSwitchArgument(CLI, Combine3D, Output);
+            
+            OpenInputFile(Lefteye,  GetCLIArgumentResult(CLI, LeftEyeArg));
+            OpenInputFile(Righteye, GetCLIArgumentResult(CLI, RightEyeArg));
+            OpenOutputFile(Stereo,  GetCLIArgumentResult(CLI, StereoArg));
+        } else if (GetCLISwitchPresence(CLI, Split3D) == true) {
+            BitInput  *Stereo     = InitBitInput();
+            BitOutput *Lefteye    = InitBitOutput();
+            BitOutput *Righteye   = InitBitOutput();
+            
+            uint64_t StereoArg    = GetCLIMetaSwitchArgument(CLI, Split3D, Output);
+            uint64_t LeftEyeArg   = GetCLIMetaSwitchArgument(CLI, Split3D, LeftEye);
+            uint64_t RightEyeArg  = GetCLIMetaSwitchArgument(CLI, Split3D, RightEye);
+            
+            OpenInputFile(Stereo, GetCLIArgumentResult(CLI, StereoArg));
+            OpenOutputFile(Lefteye, GetCLIArgumentResult(CLI, LeftEyeArg));
+            OpenOutputFile(Righteye, GetCLIArgumentResult(CLI, RightEyeArg));
+        } else {
+            // 2D image
+            BitInput  *InputFile  = InitBitInput();
+            BitOutput *OutputFile = InitBitOutput();
+            
+            char       InputPath  = GetCLIArgumentResult(CLI, Input);
+            char      OutputPath  = GetCLIArgumentResult(CLI, Output);
+            
+            OpenInputFile(InputFile, &InputPath);
+            OpenOutputFile(OutputFile, &OutputPath);
+        }
         
+        /*
         // We should add a function to BitIO that can tell us if an input command is a network address or a file.
-        
-        ParseCommandLineArguments(CMD, argc, argv);
-        if        (GetCMDSwitchPresence(CMD, 2) == true) {
+        if        (GetCLISwitchPresence(CLI, 2) == true) {
             // Split string into resolution.
-        } else if (GetCMDSwitchPresence(CMD, 3) == true) { // Encode
+        } else if (GetCLISwitchPresence(CLI, Encode) == true) { // Encode
             EncodePNG     *Enc   = InitEncodePNG();
             PNGEncodeImage(Enc, BitO);
-        } else if (GetCMDSwitchPresence(CMD, 4) == true) { // Decode
+        } else if (GetCLISwitchPresence(CLI, Decode) == true) { // Decode
             DecodePNG     *Dec   = InitDecodePNG();
             PNGDecodeImage(InputBitB, Dec, NULL);
         }
+         */
         
-        CloseCommandLineOptions(CMD);
+        DeinitCommandLineInterface(CLI);
         
         return EXIT_SUCCESS;
     }
