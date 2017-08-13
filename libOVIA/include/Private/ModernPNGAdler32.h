37cc86378008e691c20fa547b88410bafc50e8c3
Marcus Johnson <bumblebritches57@gmail.com>
2017-08-13 05:00:48 -0400

    Moved Adler32 stuff here from BitIO

---
+++ libOVIA/include/Private/ModernPNGAdler32.h
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#pragma once
+
+#ifndef LIBMODERNPNG_ModernPNGAdler32_H
+#define LIBMODERNPNG_ModernPNGAdler32_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    /*!
+     @abstract                                    "Creates Adler32 checksum from input data".
+     @return                                      "Returns the Adler32 data from the data input".
+     @param             Data                      "Pointer to the data to generate the Adler hash from".
+     @param             DataSize                  "Size of data".
+     */
+    uint32_t            GenerateAdler32(const uint8_t *Data, const uint64_t DataSize);
+    
+    /*!
+     @abstract                                    "Generates Adler32 from the input data, and compares it to the submitted checksum".
+     @return                                      "Returns whether the input data matched the provided checksum or not".
+     @param             Data                      "Pointer to the data to generate the Adler hash from".
+     @param             DataSize                  "Size of data".
+     @param             EmbeddedAdler32           "Embedded Adler32 to compare the generated one to".
+     */
+    bool                VerifyAdler32(const uint8_t *Data, const uint64_t DataSize, const uint32_t EmbeddedAdler32);
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIBMODERNPNG_ModernPNGAdler32_H */
